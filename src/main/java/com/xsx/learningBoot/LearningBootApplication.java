b859706ee5e33db71caff6a02b3b6d19b1530892
xiashixiong <xiashixiong@xgqq.com>
2018-10-09 10:52:17 +0800

    初始化init

---
+++ src/main/java/com/xsx/learningBoot/LearningBootApplication.java
@@ -0,0 +1,12 @@
+package com.xsx.learningBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBootApplication.class, args);
+	}
+}
